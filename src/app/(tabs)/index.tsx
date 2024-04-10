a935f92e9ec74ff352e6a1e0298b34b5f51fb7a6
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-04-11 03:11:57 +0500

    feat: add custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,12 @@
-import { Image, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
+import { View } from '@/src/components/Themed';
 import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri:product.image}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View style={{}}>
+     <ProductListItem product={products[0]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-   backgroundColor:"white",
-   padding:10,
-   borderRadius:20
-  },
-  image:{
-    width: "100%",
-    aspectRatio:1,
-    borderRadius: 100,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:10
-  },
-  price:{
-    color:Colors.light.tint,
-    fontWeight: 'bold',
-  }
-});
