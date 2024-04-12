e296189f5b4953f14aba1d4518948084ea12ddb1
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-04-12 13:00:19 +0500

    feat: product completed

--- src/Provider/CartProvider.tsx
+++ src/Provider/CartProvider.tsx
@@ -1,17 +1,18 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
 import { CartItem, Product } from "../types";
-import {randomUUID} from 'expo-crypto'
+import { randomUUID } from 'expo-crypto'
 
 type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: CartItem['size']) => void;
     updateQuantity: (itemId: string, amount: -1 | 1) => void;
-
+    total: number;
 }
 export const CartContext = createContext<CartType>({
     items: [],
     addItem: () => { },
-    updateQuantity: () => {}
+    updateQuantity: () => { },
+    total: 0
 });
 
 
@@ -22,8 +23,8 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         // If already in the cart, just update the quantity
 
         const existingItem = items.find(item => item.product === product && item.size === size);
-        if(existingItem) {
-            updateQuantity(existingItem.id,1);
+        if (existingItem) {
+            updateQuantity(existingItem.id, 1);
             return;
         }
 
@@ -39,20 +40,20 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     }
 
     const updateQuantity = (itemId: string, amount: -1 | 1) => {
-        const newItems = items.map((item)=> item.id !== itemId ? item : {
+        const newItems = items.map((item) => item.id !== itemId ? item : {
             ...item, quantity: item.quantity + amount
-        }).filter((item)=>item.quantity > 0);
+        }).filter((item) => item.quantity > 0);
         console.log("🚀 ~ newItems ~ newItems:", newItems)
         setItems(newItems);
     }
-
-    
+    const total = Number(items.reduce((total, item) => total + item.product.price * item.quantity, 0).toFixed(2));
 
     return (
         <CartContext.Provider value={{
             items,
             addItem,
-            updateQuantity
+            updateQuantity,
+            total
         }}>
             {children}
         </CartContext.Provider>
--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,19 +3,28 @@ import React, { useContext } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { CartContext, useCart } from '../Provider/CartProvider';
 import CartListItem from '../components/CartListItem';
+import Button from '../components/Button';
 
 const CartScreen = () => {
-    const { items } = useCart();
-    console.log("🚀 ~ ProductDetailScreen ~ items:", items)
+    const { items, total } = useCart();
+
     return (
-        <View>
+        <View style={{ padding: 10 }}>
             <FlatList data={items}
                 renderItem={({ item }) => <CartListItem cartItem={item} />}
                 contentContainerStyle={{
-                    padding: 10,
                     gap: 10
                 }}
             />
+            {
+                total > 0 ? (
+                    <>
+                        <Text style={styles.totalText}>Total: ${total}</Text>
+                        <Button text='Checkout' onPress={() => { }} />
+                    </>
+                ) : null
+            }
+
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
     )
@@ -23,4 +32,10 @@ const CartScreen = () => {
 
 export default CartScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    totalText: {
+        fontSize: 20,
+        fontWeight: '500',
+        marginVertical: 10,
+    },
+})
