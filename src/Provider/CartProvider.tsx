c6e5ff70bcb7215ca1a0d19379e98de8f1739c0a
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-04-12 12:21:26 +0500

    feat: add update shopping cart"

--- src/Provider/CartProvider.tsx
+++ src/Provider/CartProvider.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
 import { CartItem, Product } from "../types";
-
+import {randomUUID} from 'expo-crypto'
 
 type CartType = {
-    items: CartItem[],
-    addItem: (product: Product, size: CartItem['size']) => void
+    items: CartItem[];
+    addItem: (product: Product, size: CartItem['size']) => void;
+    updateQuantity: (itemId: string, amount: -1 | 1) => void;
+
 }
 export const CartContext = createContext<CartType>({
     items: [],
-    addItem: () => { }
+    addItem: () => { },
+    updateQuantity: () => {}
 });
 
 
@@ -18,14 +21,29 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     const addItem = (product: Product, size: CartItem['size']) => {
         // If already in the cart, just update the quantity
 
+        const existingItem = items.find(item => item.product === product && item.size === size);
+        if(existingItem) {
+            updateQuantity(existingItem.id,1);
+            return;
+        }
+
         const newCartItem = {
-            id: '1',
+            id: randomUUID(),
             product,
             product_id: product.id,
             size,
             quantity: 1
         };
+        console.log("🚀 ~ addItem ~ newCartItem:", newCartItem)
         setItems([newCartItem, ...items]);
+    }
+
+    const updateQuantity = (itemId: string, amount: -1 | 1) => {
+        const newItems = items.map((item)=> item.id !== itemId ? item : {
+            ...item, quantity: item.quantity + amount
+        }).filter((item)=>item.quantity > 0);
+        console.log("🚀 ~ newItems ~ newItems:", newItems)
+        setItems(newItems);
     }
 
     
@@ -33,7 +51,8 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     return (
         <CartContext.Provider value={{
             items,
-            addItem
+            addItem,
+            updateQuantity
         }}>
             {children}
         </CartContext.Provider>
